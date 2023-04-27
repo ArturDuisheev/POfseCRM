416bacafa1db1fe124a486a66a169e1b60563dcb
GGduisheevArturGG <107888994+ArturDuisheev@users.noreply.github.com>
2023-04-27 23:35:49 +0600

    Add all

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-# router.register(r'register', )
 router.register(r'client', views.ClientViewSet)
 router.register(r'project', views.ProjectViewSet)
 
